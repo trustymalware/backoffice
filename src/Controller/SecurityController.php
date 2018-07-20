243efd623a80c028bd69ef3117ff9e8bd7c3df13
trustymalware <mehdi.benseghir@trustymalware.info>
2018-07-20 14:19:30 +0200

    ...

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/connexion", name="connexion")
+     */
+    public function connexion(Request $request, AuthenticationUtils $authenticationUtils)
+    {
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/connexion.html.twig', array(
+        'last_username' => $lastUsername,
+        'error'         => $error,
+        ));
+    }
+    /**
+     * La route pour se deconnecter.
+     * 
+     * Mais celle ci ne doit jamais être executé car symfony l'interceptera avant.
+     *
+     *
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+
+    //
+     /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin()
+    {
+        return $this->render('admin/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+
+       //
+     /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('user/index.html.twig', [
+        ]);
+    }
+
+}
